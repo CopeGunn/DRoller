5f29015e142bb23ed19437e076578c423271ea70
Cope Gunn <naijanter@live.com>
2021-01-19 15:16:44 -0300

    Added "easterEgg" method
    easterEgg method gets triggered when user inputs "420" in the program options, it outputs a messages at random

--- Menu/Menu.java
+++ Menu/Menu.java
@@ -57,17 +57,12 @@ public class Menu {
                 break;
 
             case "420":
-                System.out.println(
-                    "Congratulations! you have found my Easter Egg!\n" +
-                    "Behind the angelical mother hids the root of all goods and evils\n" +
-                    "And when you reach for it, don't you dare to disrespect not a single one\n" +
-                    "of your masters, as they are the five truest most faithful sources of\n" +
-                    "knowledge of the self and beyond, don't be wise, be truth, no for they, but for thee\n");
+                easterEgg();
                 cont = true;
                 break;
 
             case "exit":
-                System.out.println("Thanks for using my shitty software c:")
+                System.out.println("Thanks for using my shitty software c:");
                 cont = false;
                 break;
 
@@ -80,10 +75,33 @@ public class Menu {
     }
 
         public static void diceThrow(int sides){
-        Random random = new Random();
-        //Valor de 'answ' igual a random limitado por 'sides'
-        int answ = random.nextInt(sides);
-        //Imprimir answ + 1 (para descartar el 0)
-        System.out.println(answ + 1);
+            Random random = new Random();
+            //Valor de 'answ' igual a random limitado por 'sides'
+            int answ = random.nextInt(sides);
+            //Imprimir answ + 1 (para descartar el 0)
+            System.out.println(answ + 1);
+    }
+        public static void easterEgg(){
+            Random random = new Random();
+            int randEgg = random.nextInt(2);
+            switch (randEgg) {
+
+            case 0:
+                System.out.println(
+                        "Congratulations! you have found my Easter Egg!\n" +
+                        "Behind the angelical mother hids the root of all goods and evils\n" +
+                        "And when you reach for it, don't you dare to disrespect not a single one\n" +
+                        "of your masters, as they are the five truest most faithful sources of\n" +
+                        "knowledge of the self and beyond, don't be wise, be truth, no for they, but for thee\n");
+                break;
+
+            case 1:
+                System.out.println("Nothing to see here cawbwoah!");
+                break;
+
+            }
+            
+
+
     }
 }
